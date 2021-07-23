057abd116953ce213ba73bee416c1c6838652cdc
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-07-23 11:02:34 +0700

    edit footer component

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>
-        <p>This is footer</p>
+    <div class="p-5 text-center">
+        <div>
+            <p class="font-semibold">
+                Made by
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="http://raflyfahrezi.space"
+                    class="text-unique"
+                >
+                    Farhan Rafly Fahrezi Saepulloh
+                </a>
+            </p>
+        </div>
+        <div class="mt-2">
+            <a href="http://github.com" class="text-gray-500 text-xs underline"
+                >Source Code</a
+            >
+        </div>
     </div>
 </template>
