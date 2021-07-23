3c0bb1c1dcbb79645d4e3b113cadcf0f2bb9e3ca
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-07-23 10:45:36 +0700

    adding tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            textColor: {
+                unique: '#00c58f',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
