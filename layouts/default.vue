1db846fb581123eafbb51c1569462869e4a548dd
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-07-23 10:10:26 +0700

    edit pageWrappe

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <LayoutNavigation />
-        <Nuxt />
+        <div class="w-full max-w-screen-lg mx-auto bg-gray-200">
+            <Nuxt />
+        </div>
         <LayoutFooter />
     </div>
 </template>
