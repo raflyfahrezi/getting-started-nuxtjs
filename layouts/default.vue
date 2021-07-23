96781f01957c939557f34d49ab09f1fd3c5b1506
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-07-23 10:54:19 +0700

    edit layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,10 @@
 <template>
-    <div>
-        <LayoutNavigation />
-        <div class="w-full max-w-screen-lg mx-auto">
-            <Nuxt />
+    <div class="min-h-screen grid grid-rows-auto">
+        <div>
+            <LayoutNavigation />
+            <div class="w-full max-w-screen-lg mx-auto">
+                <Nuxt />
+            </div>
         </div>
         <LayoutFooter />
     </div>
