2c6af3617438436431a4a737c31861258df0d71d
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-07-23 10:42:52 +0700

    layouting navigation component

--- components/layout/Navigation.vue
+++ components/layout/Navigation.vue
@@ -1,9 +1,36 @@
 <template>
-    <div>
-        <p>Test</p>
+    <div class="w-full py-3 px-5 shadow-md flex justify-between items-center">
+        <AssetsNuxtLogo class="h-8" />
+        <div class="flex">
+            <NuxtLink
+                class="ml-1 p-3"
+                v-bind:key="item"
+                v-bind:to="item.url"
+                v-for="item in navigationItem"
+            >
+                <p>
+                    {{ item.text }}
+                </p>
+            </NuxtLink>
+        </div>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            navigationItem: [
+                {
+                    url: '/',
+                    text: 'Home',
+                },
+                {
+                    url: '/about',
+                    text: 'About',
+                },
+            ],
+        }
+    },
+}
 </script>
