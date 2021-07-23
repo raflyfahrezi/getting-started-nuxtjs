bf83396241157304c21f96e49be01ae7d7ee087d
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-07-23 11:08:10 +0700

    fix layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,10 @@
     <div class="min-h-screen grid grid-rows-auto">
         <div>
             <LayoutNavigation />
-            <div class="w-full max-w-screen-lg mx-auto">
-                <Nuxt />
+            <div class="w-full flex justify-center p-5">
+                <div class="w-full max-w-screen-lg">
+                    <Nuxt />
+                </div>
             </div>
         </div>
         <LayoutFooter />
