c2f650b35296a557f23f794f244853fc51322682
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-07-26 13:23:25 +0700

    fetching data

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,11 @@
         <div class="text-center my-10">
             <h1 class="text-5xl font-bold">Welcome to Nuxt.js</h1>
         </div>
-        <div></div>
+        <div>
+            <div v-for="user in users" :key="user.id">
+                {{ user.name }}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,6 +17,16 @@ export default {
         return {
             title: 'Home | Getting Started with Nuxtjs',
         }
+    },
+    data() {
+        return {
+            users: [],
+        }
+    },
+    async fetch() {
+        this.users = await fetch(
+            'https://jsonplaceholder.typicode.com/users'
+        ).then((res) => res.json())
     },
 }
 </script>
