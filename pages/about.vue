fdc56dc30290ac1e9ed2eee55a5fe6fb308fdcb0
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-07-26 13:40:06 +0700

    create about pages

---
+++ pages/about.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="text-center my-10">
+            <h1 class="text-5xl font-bold">About</h1>
+        </div>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi,
+            laboriosam animi ad nostrum odio similique. Sunt hic aspernatur
+            animi nesciunt laudantium enim iusto nemo illo ea fuga magni ipsa
+            consequatur necessitatibus quos quas, rem unde ut reprehenderit,
+            magnam, eveniet soluta doloribus recusandae culpa. Minus dolorum
+            dicta consequatur amet nulla? Iste?
+        </p>
+        <br />
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Voluptatibus eius, voluptates velit officia eligendi aliquid impedit
+            ea nam nulla amet deleniti esse quod aut aspernatur labore unde.
+            Blanditiis ipsa labore cum sunt quisquam, dicta at sed obcaecati
+            harum perferendis natus, tempora dolorum est culpa. Maxime illo quos
+            nemo temporibus doloremque velit rem vitae fugit nesciunt aperiam
+            eveniet enim, sunt reprehenderit.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'About | Getting Started with Nuxtjs',
+        }
+    },
+}
+</script>
