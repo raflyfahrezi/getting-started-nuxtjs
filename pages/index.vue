fb3296bb5e2755ff393ecbca64c1641358435641
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-07-26 13:02:37 +0700

    adding header

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-    <div><p>This is index</p></div>
+    <div>
+        <div class="text-center my-10">
+            <h1 class="text-5xl font-bold">Welcome to Nuxt.js</h1>
+        </div>
+        <div></div>
+    </div>
 </template>
 
 <script>
